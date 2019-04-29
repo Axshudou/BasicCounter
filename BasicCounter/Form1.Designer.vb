f9cbc2dd2886d5452cb2d91bad46519051c9d9a4
Axoudu80 <axoudu80@gmail.com>
2019-04-29 15:38:26 +0200

    Ajout 3 projets

--- BasicCounter/Form1.Designer.vb
+++ BasicCounter/Form1.Designer.vb
@@ -22,10 +22,53 @@ Partial Class Form1
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.decrement = New System.Windows.Forms.Button()
+        Me.increment = New System.Windows.Forms.Button()
+        Me.RAZ = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'decrement
+        '
+        Me.decrement.Location = New System.Drawing.Point(67, 66)
+        Me.decrement.Name = "decrement"
+        Me.decrement.Size = New System.Drawing.Size(75, 23)
+        Me.decrement.TabIndex = 0
+        Me.decrement.Text = "-"
+        Me.decrement.UseVisualStyleBackColor = True
+        '
+        'increment
+        '
+        Me.increment.Location = New System.Drawing.Point(238, 66)
+        Me.increment.Name = "increment"
+        Me.increment.Size = New System.Drawing.Size(75, 23)
+        Me.increment.TabIndex = 1
+        Me.increment.Text = "+"
+        Me.increment.UseVisualStyleBackColor = True
+        '
+        'RAZ
+        '
+        Me.RAZ.Location = New System.Drawing.Point(149, 151)
+        Me.RAZ.Name = "RAZ"
+        Me.RAZ.Size = New System.Drawing.Size(75, 23)
+        Me.RAZ.TabIndex = 2
+        Me.RAZ.Text = "RAZ"
+        Me.RAZ.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(379, 232)
+        Me.Controls.Add(Me.RAZ)
+        Me.Controls.Add(Me.increment)
+        Me.Controls.Add(Me.decrement)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents decrement As Button
+    Friend WithEvents increment As Button
+    Friend WithEvents RAZ As Button
 End Class
